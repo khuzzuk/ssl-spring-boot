a6c8ebcd370290a79a3d75ac4fc0136f00331ad6
Adrian Drabik <adriandrabik@gmail.com>
2018-11-27 07:49:17 +0100

    switch to pkcs12

--- src/main/java/com/example/sslspringboot/controller/Controller.java
+++ src/main/java/com/example/sslspringboot/controller/Controller.java
@@ -1,11 +1,12 @@
 package com.example.sslspringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class Controller {
     @GetMapping("data")
     public List<String> data() {
